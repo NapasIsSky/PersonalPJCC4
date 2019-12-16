a40f2b95a82824dab677b23b86ec76add2e5a15a
NAPAS KAVALEE <napas.kavalee@gmail.com>
2019-12-16 18:52:44 +0700

    16DEC2019 1852 fontend add page

--- fontend/src/App.js
+++ fontend/src/App.js
@@ -1,22 +1,10 @@
 import React from 'react';
-import './App.css';
+import LoginPage from './component/login'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoginPage/>
     </div>
   );
 }
